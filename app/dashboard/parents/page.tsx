7ea5fcba1843cc629619f649894da7067724141b
huiting <chtmn678@gmail.com>
2024-02-27 13:05:40 +0800

    update parent page

--- app/dashboard/parents/page.tsx
+++ app/dashboard/parents/page.tsx
@@ -67,20 +67,19 @@ export default async function Page({
 
   return (
     <div className="w-full">
-        家长野牦牛
-      {/*<div className="flex w-full items-center justify-between">*/}
-      {/*  <h1 className={`${lusitana.className} text-2xl`}>Parents</h1>*/}
-      {/*</div>*/}
-      {/*<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">*/}
-      {/*  <Search placeholder="Search parents..." />*/}
-      {/*  <CreateParent />*/}
-      {/*</div>*/}
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Parents</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search parents..." />
+        <CreateParent />
+      </div>
       {/*<Suspense key={query + currentPage} fallback={<ParentsTableSkeleton />}>*/}
       {/*  <Table query={query} currentPage={currentPage} schoolName={schoolName} />*/}
       {/*</Suspense>*/}
-      {/*<div className="mt-5 flex w-full justify-center">*/}
-      {/*  <Pagination totalPages={totalPagesOrDefault} />*/}
-      {/*</div>*/}
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPagesOrDefault} />
+      </div>
     </div>
   );
 }
