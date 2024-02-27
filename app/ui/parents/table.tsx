f655c5fa6f416d71962546d2d581d4f2b65c4523
huiting <chtmn678@gmail.com>
2024-02-27 13:32:35 +0800

    update parent page

--- app/ui/parents/table.tsx
+++ app/ui/parents/table.tsx
@@ -19,48 +19,48 @@ export default async function ParentsTable({
   let students: any[] = [];
   let combinedData: any[] = []; // Initialize combinedData here
 
-  // try {
-  //   parents = await fetchFilteredParents(query, currentPage, schoolName);
-  //   parentEmails = parents.map((parent) => parent.email);
-  //   students = await fetchStudentsByParentsEmail(parentEmails);
-  //
-  //   const studentFirstNamesByParentEmail: any[] = [];
-  //
-  //   for (const studentEntry of students) {
-  //     const studentFirstNames: string[] = [];
-  //
-  //     for (const studentId of studentEntry.studentIds) {
-  //       const student = await fetchStudentNameById(studentId);
-  //       if (student) {
-  //         studentFirstNames.push(student.name);
-  //       }
-  //     }
-  //
-  //     studentFirstNamesByParentEmail.push({
-  //       email: studentEntry.email,
-  //       student_name: studentFirstNames,
-  //     });
-  //   }
-  //
-  //   combinedData = parents.map((parent) => {
-  //     const studentNames =
-  //       studentFirstNamesByParentEmail.find(
-  //         (entry) => entry.email === parent.email
-  //       )?.student_name || [];
-  //     return {
-  //       _id: parent._id,
-  //       email: parent.email,
-  //       name: `${parent.firstName} ${parent.lastName}`,
-  //       phoneNum: parent.phoneNum,
-  //       students: studentNames,
-  //     };
-  //   });
-  //
-  //   console.log('Combined Data:', combinedData);
-  // } catch (error: any) {
-  //   console.error('Error fetching parents:', error);
-  //   // You can return an error message or a loading spinner here if needed
-  // }
+  try {
+    parents = [] || await fetchFilteredParents(query, currentPage, schoolName);
+    parentEmails = parents.map((parent) => parent.email);
+    students = await fetchStudentsByParentsEmail(parentEmails);
+
+    const studentFirstNamesByParentEmail: any[] = [];
+
+    for (const studentEntry of students) {
+      const studentFirstNames: string[] = [];
+
+      for (const studentId of studentEntry.studentIds) {
+        const student = await fetchStudentNameById(studentId);
+        if (student) {
+          studentFirstNames.push(student.name);
+        }
+      }
+
+      studentFirstNamesByParentEmail.push({
+        email: studentEntry.email,
+        student_name: studentFirstNames,
+      });
+    }
+
+    combinedData = parents.map((parent) => {
+      const studentNames =
+        studentFirstNamesByParentEmail.find(
+          (entry) => entry.email === parent.email
+        )?.student_name || [];
+      return {
+        _id: parent._id,
+        email: parent.email,
+        name: `${parent.firstName} ${parent.lastName}`,
+        phoneNum: parent.phoneNum,
+        students: studentNames,
+      };
+    });
+
+    console.log('Combined Data:', combinedData);
+  } catch (error: any) {
+    console.error('Error fetching parents:', error);
+    // You can return an error message or a loading spinner here if needed
+  }
 
   return (
     <div className="mt-6 flow-root">
@@ -88,37 +88,37 @@ export default async function ParentsTable({
               </tr>
             </thead>
             <tbody className="bg-white">
-              {/*{combinedData.map((parent) => (*/}
-              {/*  <tr*/}
-              {/*    key={parent._id}*/}
-              {/*    className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"*/}
-              {/*  >*/}
-              {/*    <td className="whitespace-nowrap py-3 pl-6 pr-3">*/}
-              {/*      <Link href={`/dashboard/parents/${parent._id}`}>*/}
-              {/*        <span className="text-blue-600 cursor-pointer underline">*/}
-              {/*          <p>{parent.email}</p>*/}
-              {/*        </span>*/}
-              {/*      </Link>*/}
-              {/*    </td>*/}
-              {/*    <td className="whitespace-nowrap px-3 py-3">*/}
-              {/*      {parent.name}*/}
-              {/*    </td>*/}
-              {/*    <td className="whitespace-nowrap px-3 py-3">*/}
-              {/*      {parent.phoneNum}*/}
-              {/*    </td>*/}
-              {/*    <td className="whitespace-nowrap px-3 py-3">*/}
-              {/*      {parent.students.map((student: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined, index: Key | null | undefined) => (*/}
-              {/*        <p key={index}>{student}</p>*/}
-              {/*      ))}*/}
-              {/*    </td>*/}
-              {/*    <td className="whitespace-nowrap py-3 pl-6 pr-3">*/}
-              {/*      <div className="flex justify-end gap-3">*/}
-              {/*        <UpdateParent id={parent._id} />*/}
-              {/*        <DeleteParent id={parent._id} />*/}
-              {/*      </div>*/}
-              {/*    </td>*/}
-              {/*  </tr>*/}
-              {/*))}*/}
+              {combinedData.map((parent) => (
+                <tr
+                  key={parent._id}
+                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                >
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <Link href={`/dashboard/parents/${parent._id}`}>
+                      <span className="text-blue-600 cursor-pointer underline">
+                        <p>{parent.email}</p>
+                      </span>
+                    </Link>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {parent.name}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {parent.phoneNum}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {parent.students.map((student: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined, index: Key | null | undefined) => (
+                      <p key={index}>{student}</p>
+                    ))}
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex justify-end gap-3">
+                      <UpdateParent id={parent._id} />
+                      <DeleteParent id={parent._id} />
+                    </div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
           {combinedData.length === 0 && (
