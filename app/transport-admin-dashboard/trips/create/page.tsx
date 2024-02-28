a243d9208efdbfde3173ba157a89b1b99a556894
huiting <chtmn678@gmail.com>
2024-02-28 14:47:30 +0800

    update trip

--- app/transport-admin-dashboard/trips/create/page.tsx
+++ app/transport-admin-dashboard/trips/create/page.tsx
@@ -1,37 +1,37 @@
-// import Form from '@/app/ui/trips/create-form';
+import Form from '@/app/ui/trips/create-form';
 import Breadcrumbs from '@/app/ui/trips/breadcrumbs';
-// import {fetchAllDriversEmails, fetchAllVehicleIds, fetchCompanyName, fetchDataForCreateTrips} from '@/app/lib/data3';
-// import { fetchSessionToken } from '@/app/lib/data';
-// import jwt, { JwtPayload } from 'jsonwebtoken';
+import {fetchAllDriversEmails, fetchAllVehicleIds, fetchCompanyName, fetchDataForCreateTrips} from '@/app/lib/data3';
+import { fetchSessionToken } from '@/app/lib/data';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export default async function Page() {
-  // //Fetch session token
-  // const sessionName = 'currentSession'; // Adjust session name according to your setup
-  //   console.log('huoqu token33333333333333333333333333333333')
-  // const token = await fetchSessionToken(sessionName);
-  // console.log('Session token111111111111111111111111111111111111:', token);
-  //   console.log('获取token陈工',token)
-  // // Verify and decode the token
-  // let decodedToken: JwtPayload | string; // Explicitly type decodedToken
-  // try {
-  //   // Type assertion to assert that token is a non-null string
-  //   decodedToken = jwt.verify(token!, process.env.TOKEN_SECRET!) as JwtPayload;
-  //   console.log('Decoded token data22222222222222222222222222222:', decodedToken);
-  // } catch (error) {
-  //   console.error('Error verifying token:', error);
-  //   // Handle error if token verification fails or token is null
-  //   return null; // Or handle the error in some other way
-  // }
-  //   console.log('token解析陈工',decodedToken)
-  //   const sessionUserId = typeof decodedToken === 'string' ? decodedToken : decodedToken?.id;
-  //   console.log('sessionUserId值：',sessionUserId)
-  //   console.log('开始调用fetchDataForCreateTrips')
-  //   const {vehicles,drivers} = await fetchDataForCreateTrips(sessionUserId)
-  //   console.log('fetchDataForCreateTrips调用成功',vehicles,drivers)
-  // //
-  // // // Extract user ID from decoded token
-  // // const sessionUserId = typeof decodedToken === 'string' ? decodedToken : decodedToken?.id;
-  // //
+  //Fetch session token
+  const sessionName = 'currentSession'; // Adjust session name according to your setup
+    console.log('huoqu token33333333333333333333333333333333')
+  const token = await fetchSessionToken(sessionName);
+  console.log('Session token111111111111111111111111111111111111:', token);
+    console.log('获取token陈工',token)
+  // Verify and decode the token
+  let decodedToken: JwtPayload | string; // Explicitly type decodedToken
+  try {
+    // Type assertion to assert that token is a non-null string
+    decodedToken = jwt.verify(token!, process.env.TOKEN_SECRET!) as JwtPayload;
+    console.log('Decoded token data22222222222222222222222222222:', decodedToken);
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    // Handle error if token verification fails or token is null
+    return null; // Or handle the error in some other way
+  }
+    console.log('token解析陈工',decodedToken)
+    const sessionUserId = typeof decodedToken === 'string' ? decodedToken : decodedToken?.id;
+    console.log('sessionUserId值：',sessionUserId)
+    console.log('开始调用fetchDataForCreateTrips')
+    // const {vehicles,drivers} = await fetchDataForCreateTrips(sessionUserId)
+    // console.log('fetchDataForCreateTrips调用成功',vehicles,drivers)
+  //
+  // // Extract user ID from decoded token
+  // const sessionUserId = typeof decodedToken === 'string' ? decodedToken : decodedToken?.id;
+  //
   // // // Fetch the company name using the user ID
   // // const companyName = await fetchCompanyName(sessionUserId);
   // // console.log('Company Name:', companyName);
@@ -54,7 +54,8 @@ console.error('你好好你好好你好好你好好')
             active: true,
           },
         ]}
-      />这是新的
+      />
+        sessionUserId: {sessionUserId}
       {/*<Form drivers={drivers} vehicles={vehicles} />*/}
     </main>
   );
